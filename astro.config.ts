4ac02c0a3b5b7fdf403ca66776d090e4389b76e4
Kauê Fraga Rodrigues <88486000+kauefraga@users.noreply.github.com>
2024-08-28 22:37:28 -0300

    chore: add wrangler to deploy on cloudflare

--- astro.config.ts
+++ astro.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://kauefraga.netlify.app',
+  site: 'https://kauefraga.pages.dev',
   integrations: [
     react(),
     tailwind(),
