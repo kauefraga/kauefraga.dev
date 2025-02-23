8638bcb623225a874e494379662673b885524d67
Kauê Fraga Rodrigues <88486000+kauefraga@users.noreply.github.com>
2025-02-23 16:05:08 -0300

    feat: remove react plugin

--- astro.config.ts
+++ astro.config.ts
@@ -1,4 +1,3 @@
-import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
 import { defineConfig } from 'astro/config';
@@ -6,7 +5,6 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://kauefraga.dev',
   integrations: [
-    react(),
     tailwind(),
     icon({
       include: {
